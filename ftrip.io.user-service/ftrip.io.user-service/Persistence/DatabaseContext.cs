64f66953bb33180b1eb22dcad0026cde7132a299
Panic <panic.milos99@gmail.com>
2023-04-19 20:16:04 +0200

    feat: framework setup and dockerfiles

---
+++ ftrip.io.user-service/ftrip.io.user-service/Persistence/DatabaseContext.cs
@@ -0,0 +1,14 @@
+﻿using ftrip.io.framework.Contexts;
+using ftrip.io.framework.Persistence.Sql.Database;
+using Microsoft.EntityFrameworkCore;
+
+namespace ftrip.io.user_service.Persistence
+{
+    public class DatabaseContext : DatabaseContextBase<DatabaseContext>
+    {
+        public DatabaseContext(DbContextOptions<DatabaseContext> options, CurrentUserContext currentUserContext) :
+            base(options, currentUserContext)
+        {
+        }
+    }
+}
--- ftrip.io.user-service/ftrip.io.user-service/Program.cs
+++ ftrip.io.user-service/ftrip.io.user-service/Program.cs
@@ -1,11 +1,7 @@
+using ftrip.io.framework.Persistence.Sql.Migrations;
+using ftrip.io.user_service.Persistence;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace ftrip.io.user_service
 {
@@ -13,7 +9,10 @@ namespace ftrip.io.user_service
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            CreateHostBuilder(args)
+                .Build()
+                .MigrateDbContext<DatabaseContext>()
+                .Run();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
