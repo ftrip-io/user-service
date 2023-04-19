25d564600d0c0d3d2aca90440de58438cc9ce919
Panic <panic.milos99@gmail.com>
2023-04-19 20:16:04 +0200

    feat: framework setup and dockerfiles

--- ftrip.io.user-service/ftrip.io.user-service/Startup.cs
+++ ftrip.io.user-service/ftrip.io.user-service/Startup.cs
@@ -1,15 +1,19 @@
+using ftrip.io.framework.auth;
+using ftrip.io.framework.CQRS;
+using ftrip.io.framework.ExceptionHandling.Extensions;
+using ftrip.io.framework.Globalization;
+using ftrip.io.framework.Installers;
+using ftrip.io.framework.Mapping;
+using ftrip.io.framework.messaging.Installers;
+using ftrip.io.framework.Persistence.Sql.Mariadb;
+using ftrip.io.framework.Swagger;
+using ftrip.io.framework.Validation;
+using ftrip.io.user_service.Persistence;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace ftrip.io.user_service
 {
@@ -26,6 +30,17 @@ namespace ftrip.io.user_service
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+
+            InstallerCollection.With(
+                new SwaggerInstaller<Startup>(services),
+                new AutoMapperInstaller<Startup>(services),
+                new FluentValidationInstaller<Startup>(services),
+                new GlobalizationInstaller<Startup>(services),
+                new JwtAuthenticationInstaller(services),
+                new MariadbInstaller<DatabaseContext>(services),
+                new CQRSInstaller<Startup>(services),
+                new RabbitMQInstaller<Startup>(services, RabbitMQInstallerType.Publisher | RabbitMQInstallerType.Consumer)
+            ).Install();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -40,12 +55,17 @@ namespace ftrip.io.user_service
 
             app.UseRouting();
 
+            app.UseAuthentication();
             app.UseAuthorization();
+
+            app.UseFtripioGlobalExceptionHandler();
 
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();
             });
+
+            app.UseFtripioSwagger(Configuration.GetSection(nameof(SwaggerUISettings)).Get<SwaggerUISettings>());
         }
     }
 }
