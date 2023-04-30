ad713c83e883b8d4d8c381b7bd7ae9688407d65e
Panic <panic.milos99@gmail.com>
2023-04-30 13:18:42 +0200

    feat: #2 user authentication logic

---
+++ ftrip.io.user-service/ftrip.io.user-service/Auth/UseCases/Authenticate/AuthenticatedUser.cs
@@ -0,0 +1,10 @@
+﻿using ftrip.io.user_service.Users.Domain;
+
+namespace ftrip.io.user_service.Auth.UseCases.Authenticate
+{
+    public class AuthenticatedUser
+    {
+        public User User { get; set; }
+        public string Token { get; set; }
+    }
+}
