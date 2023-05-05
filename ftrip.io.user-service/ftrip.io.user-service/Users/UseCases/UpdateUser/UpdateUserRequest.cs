0983b225e5ec1aefa89e89890ba806c32c24aa52
Luka Bjelica <52993990+bjelicaluka@users.noreply.github.com>
2023-05-05 15:03:49 +0200

    feat: #3 implemented user profile and password change (#11)

---
+++ ftrip.io.user-service/ftrip.io.user-service/Users/UseCases/UpdateUser/UpdateUserRequest.cs
@@ -0,0 +1,18 @@
+﻿using ftrip.io.user_service.Users.Domain;
+using MediatR;
+using System;
+using System.Text.Json.Serialization;
+
+namespace ftrip.io.user_service.Users.UseCases.UpdateUser
+{
+    public class UpdateUserRequest : IRequest<User>
+    {
+        [JsonIgnore]
+        public Guid Id { get; set; }
+
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string City { get; set; }
+    }
+}
---
+++ ftrip.io.user-service/ftrip.io.user-service/Users/UseCases/UpdateUser/UpdateUserRequestValidator.cs
@@ -0,0 +1,36 @@
+﻿using FluentValidation;
+using ftrip.io.framework.Globalization;
+using ftrip.io.user_service.Common;
+
+namespace ftrip.io.user_service.Users.UseCases.UpdateUser
+{
+    public class UpdateUserRequestValidator : AbstractValidator<UpdateUserRequest>
+    {
+        public UpdateUserRequestValidator(IStringManager stringManager)
+        {
+            RuleFor(request => request.FirstName)
+                .NotEmpty()
+                .WithMessage(stringManager.Format("Common_Validation_FieldIsRequired", "First name"))
+                .Matches(RegexConstants.ALPHA_REGEX)
+                .WithMessage(stringManager.Format("Common_Validation_FieldMustContainAlphaChars", "First name"));
+
+            RuleFor(request => request.LastName)
+                .NotEmpty()
+                .WithMessage(stringManager.Format("Common_Validation_FieldIsRequired", "Last name"))
+                .Matches(RegexConstants.ALPHA_REGEX)
+                .WithMessage(stringManager.Format("Common_Validation_FieldMustContainAlphaChars", "Last name"));
+
+            RuleFor(request => request.Email)
+                .NotEmpty()
+                .WithMessage(stringManager.Format("Common_Validation_FieldIsRequired", "Email"))
+                .Matches(RegexConstants.EMAIL_REGEX)
+                .WithMessage(stringManager.Format("Common_Validation_FieldMustBeEmail", "Email"));
+
+            RuleFor(request => request.City)
+                .NotEmpty()
+                .WithMessage(stringManager.Format("Common_Validation_FieldIsRequired", "City"))
+                .Matches(RegexConstants.ALPHANUMERIC_REGEX)
+                .WithMessage(stringManager.Format("Common_Validation_FieldMustContainAlphaNumChars", "City"));
+        }
+    }
+}
