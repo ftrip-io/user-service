76595a36aa0314a8e37e6ff77ba3c6cec39aa72a
Panic <panic.milos99@gmail.com>
2023-05-08 20:31:24 +0200

    Feat/user-created-updated-events (#12)
    * feat: added and published user created/updated events.
    refactor: ftrip.ip -> ftrip.io for contracts.
    refactor: test structure for accounts.
    
    * Update README.md
    
    * feat: added Microsoft.EntityFrameworkCore.Relational
    
    * feat: updated framework version

---
+++ ftrip.io.user-service/ftrip.io.user-service.contracts/Users/Events/UserCreatedEvent.cs
@@ -0,0 +1,18 @@
+﻿using ftrip.io.framework.Domain;
+using System;
+
+namespace ftrip.io.user_service.contracts.Users.Events
+{
+    public class UserCreatedEvent : Event<string>
+    {
+        public string UserId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+
+        public UserCreatedEvent()
+        {
+            Id = Guid.NewGuid().ToString();
+        }
+    }
+}
---
+++ ftrip.io.user-service/ftrip.io.user-service.unit-tests/Accounts/UseCases/ChangePassword/ChangePasswordRequestHandlerTests.cs
@@ -0,0 +1,116 @@
+﻿using FluentAssertions;
+using ftrip.io.framework.ExceptionHandling.Exceptions;
+using ftrip.io.framework.Globalization;
+using ftrip.io.framework.Persistence.Contracts;
+using ftrip.io.user_service.Accounts;
+using ftrip.io.user_service.Accounts.Domain;
+using ftrip.io.user_service.Accounts.UseCases.ChangePassword;
+using ftrip.io.user_service.Accounts.Utilities;
+using Moq;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace ftrip.io.user_service.unit_tests.Accounts.UseCases.ChangePassword
+{
+    public class ChangePasswordRequestHandlerTests
+    {
+        private readonly Mock<IUnitOfWork> _unitOfWorkMock = new Mock<IUnitOfWork>();
+        private readonly Mock<IAccountRepository> _accountRepositoryMock = new Mock<IAccountRepository>();
+        private readonly Mock<IStringManager> _stringManagerMock = new Mock<IStringManager>();
+
+        private readonly ChangePasswordRequestHandler _handler;
+
+        public ChangePasswordRequestHandlerTests()
+        {
+            _handler = new ChangePasswordRequestHandler(
+                _unitOfWorkMock.Object,
+                _accountRepositoryMock.Object,
+                _stringManagerMock.Object
+            );
+        }
+
+        [Fact]
+        public void Handle_AccountDoesNotExist_ThrowsException()
+        {
+            // Arrange
+            var request = GetChangePasswordRequest();
+
+            // Act
+            Func<Task> handleAction = () => _handler.Handle(request, CancellationToken.None);
+
+            // Assert
+            handleAction.Should().ThrowExactlyAsync<MissingEntityException>();
+            _unitOfWorkMock.Verify(uow => uow.Commit(It.IsAny<CancellationToken>()), Times.Never);
+        }
+
+        [Fact]
+        public void Handle_CurrentPasswordDoesNotMatch_ThrowsException()
+        {
+            // Arrange
+            var request = GetChangePasswordRequest();
+
+            _accountRepositoryMock
+                .Setup(r => r.ReadByUserId(It.IsAny<Guid>(), It.IsAny<CancellationToken>()))
+                .Returns((Guid userId, CancellationToken _) =>
+                {
+                    var salt = "RandomSalt";
+                    return Task.FromResult(new CredentialsAccount()
+                    {
+                        Id = Guid.NewGuid(),
+                        UserId = userId,
+                        Salt = salt,
+                        HashedPassword = PasswordHasher.Hash("Password", salt)
+                    });
+                });
+
+            // Act
+            Func<Task> handleAction = () => _handler.Handle(request, CancellationToken.None);
+
+            // Assert
+            handleAction.Should().ThrowExactlyAsync<BadLogicException>();
+            _unitOfWorkMock.Verify(uow => uow.Commit(It.IsAny<CancellationToken>()), Times.Never);
+        }
+
+        [Fact]
+        public async Task Handle_Successful_ReturnsAccount()
+        {
+            // Arrange
+            var request = GetChangePasswordRequest();
+
+            _accountRepositoryMock
+                .Setup(r => r.ReadByUserId(It.IsAny<Guid>(), It.IsAny<CancellationToken>()))
+                .Returns((Guid userId, CancellationToken _) =>
+                {
+                    var salt = "RandomSalt";
+                    return Task.FromResult(new CredentialsAccount()
+                    {
+                        Id = Guid.NewGuid(),
+                        UserId = userId,
+                        Salt = salt,
+                        HashedPassword = PasswordHasher.Hash(request.CurrentPassword, salt)
+                    });
+                });
+
+            // Act
+            var account = await _handler.Handle(request, CancellationToken.None);
+
+            // Assert
+            account.Should().NotBeNull();
+            account.Salt.Should().NotBe("RandomSalt");
+            account.HashedPassword.Should().NotBe(request.NewPassword);
+            _unitOfWorkMock.Verify(uow => uow.Commit(It.IsAny<CancellationToken>()), Times.Once);
+        }
+
+        private ChangePasswordRequest GetChangePasswordRequest()
+        {
+            return new ChangePasswordRequest()
+            {
+                UserId = Guid.NewGuid(),
+                CurrentPassword = "Test1234@",
+                NewPassword = "Test12345@"
+            };
+        }
+    }
+}
