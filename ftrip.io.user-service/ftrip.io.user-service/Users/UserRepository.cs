7b9204f309666c4f7d850fae4b4250a3e279be26
Panic <panic.milos99@gmail.com>
2023-06-07 17:30:11 +0200

    feat: added read by ids endpoint (#17)

--- ftrip.io.user-service/ftrip.io.user-service/Users/UserRepository.cs
+++ ftrip.io.user-service/ftrip.io.user-service/Users/UserRepository.cs
@@ -3,17 +3,30 @@ using ftrip.io.framework.Persistence.Sql.Repository;
 using ftrip.io.user_service.Users.Domain;
 using Microsoft.EntityFrameworkCore;
 using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace ftrip.io.user_service.Users
 {
     public interface IUserRepository : IRepository<User, Guid>
     {
+        Task<IEnumerable<User>> ReadByIds(Guid[] ids, CancellationToken cancellationToken);
     }
 
     public class UserRepository : Repository<User, Guid>, IUserRepository
     {
         public UserRepository(DbContext context) : base(context)
         {
+        }
+
+        public async Task<IEnumerable<User>> ReadByIds(Guid[] ids, CancellationToken cancellationToken)
+        {
+            return await _entities
+                .Where(user => ids.Contains(user.Id))
+                .AsNoTracking()
+                .ToListAsync(cancellationToken);
         }
     }
 }
