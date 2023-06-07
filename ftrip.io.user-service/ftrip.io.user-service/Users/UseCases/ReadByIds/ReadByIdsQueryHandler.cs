1aaeddd312f3da785661e349d5de41d84b0e42da
Panic <panic.milos99@gmail.com>
2023-06-07 17:20:33 +0200

    feat: added read by ids endpoint

---
+++ ftrip.io.user-service/ftrip.io.user-service/Users/UseCases/ReadByIds/ReadByIdsQueryHandler.cs
@@ -0,0 +1,23 @@
+﻿using ftrip.io.user_service.Users.Domain;
+using MediatR;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ftrip.io.user_service.Users.UseCases.ReadByIds
+{
+    public class ReadByIdsQueryHandler : IRequestHandler<ReadByIdsQuery, IEnumerable<User>>
+    {
+        private readonly IUserRepository _userRepository;
+
+        public ReadByIdsQueryHandler(IUserRepository userRepository)
+        {
+            _userRepository = userRepository;
+        }
+
+        public async Task<IEnumerable<User>> Handle(ReadByIdsQuery request, CancellationToken cancellationToken)
+        {
+            return await _userRepository.ReadByIds(request.Ids, cancellationToken);
+        }
+    }
+}
