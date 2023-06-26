9a73a43639d93427efe1183a5e03949bb9eb9568
Panic <panic.milos99@gmail.com>
2023-06-26 18:08:54 +0200

    fix: #4 deleteting account with user

---
+++ ftrip.io.user-service/ftrip.io.user-service/Accounts/UseCases/DeleteAccount/DeleteAccountRequest.cs
@@ -0,0 +1,11 @@
+﻿using ftrip.io.user_service.Accounts.Domain;
+using MediatR;
+using System;
+
+namespace ftrip.io.user_service.Accounts.UseCases.DeleteAccount
+{
+    public class DeleteAccountRequest : IRequest<CredentialsAccount>
+    {
+        public Guid AccountId { get; set; }
+    }
+}
